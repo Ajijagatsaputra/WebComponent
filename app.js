05402d6864cab59c9afdb037541920372668271a
Ajijagatsaputra <ajijagat90@gmail.com>
2024-04-11 05:18:53 +0700

    webcomponent

---
+++ app.js
@@ -0,0 +1,6 @@
+import './src/script/components/index.js';
+import home from './src/script/view/home.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  home();
+});
---
+++ src/script/components/index.js
@@ -0,0 +1,2 @@
+import './card.js';
+import './note-app.js';
