75b282cfbfefc8bbb751212dd1967bd85361df2b
Ajijagatsaputra <ajijagat90@gmail.com>
2024-04-13 15:10:41 +0700

    perbaikanfooter

--- src/script/components/footer.js
+++ src/script/components/footer.js
@@ -7,18 +7,18 @@ class Footer extends HTMLElement {
         this.innerHTML = `
             <style>
                 footer {
-                    background-color: #898121;
+                    background-color: #959493;
                     color: white;
                     padding: 10px;
                     text-align: center;
                     font-weight: bolder;
                     width: 100%;
-                    height: 50px;
+                    height: 40px;
                     bottom: 0;
                 }
             </style>
             <footer>
-                <p><slot></slot></p>
+                <p><slot>Note Apps</slot></p>
             </footer>`;
     }
 }
